80c5629844e2fe42950fa467937a1baa760efbb4
lailailee <18292889055@163.com>
2019-10-06 01:16:41 +0800

    zc

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
+  }
+}
+
+module.exports = config
